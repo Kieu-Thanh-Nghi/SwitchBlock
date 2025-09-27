6eb47a723e5283cb84d5984dd40cd3b574c20be4
Kieu Thanh Nghi <souleater2222001@gmail.com>
2025-09-27 21:11:55 +0700

    ver21.7

--- Assets/_Asset/Script/LoadingMenu.cs
+++ Assets/_Asset/Script/LoadingMenu.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using TMPro;
 
 public class LoadingMenu : MonoBehaviour
 {
     [SerializeField] GameObject Menu;
+    [SerializeField] TMP_Text text;
 
     private void Start()
     {
         Invoke(nameof(LoadMenu), 0.5f);
+        logIn();
     }
 
     void LoadMenu()
     {
         Menu.SetActive(true);
+    }
+
+    public void logIn()
+    {
+        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+    }
+
+    internal void ProcessAuthentication(SignInStatus status)
+    {
+        Debug.Log(status);
+        if (status == SignInStatus.Success)
+        {
+            text.text = "Success";
+        }
+        else
+        {
+            text.text = "False";
+        }
     }
 }
